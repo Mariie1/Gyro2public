f3b72c85dcb3a384731d9e31577bc545c67bad95
Mariie1 <123565321+Mariie1@users.noreply.github.com>
2023-05-19 15:41:53 +0200

    .

--- app/src/main/java/com/example/gyro2/HostActivity.java
+++ app/src/main/java/com/example/gyro2/HostActivity.java
@@ -3,14 +3,17 @@ package com.example.gyro2;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.R;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HostActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
     }
+
 }
